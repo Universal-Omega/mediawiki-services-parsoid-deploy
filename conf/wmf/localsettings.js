560f8b48d4e2ec32c58cd789d6c449b36ee34293
Arlo Breault <abreault@wikimedia.org>
2016-11-29 12:14:38 -0800

    Can't use undefined annotations from 3fdc1a37
     * Let's revert some of c507329e instead.
    
     * scap is complaining,
    
       20:08:51 deploy failed: <ConstructorError> could not determine a
       constructor for the tag 'tag:yaml.org,2002:js/undefined'
       in "<string>", line 3, column 12:
       api_proxy: !!js/undefined ''
    
       it doesn't know about that syntax.
    
    Change-Id: Iea9658f76ff26bfd227e73d1a7c52f40ee846b85

--- conf/wmf/localsettings.js
+++ conf/wmf/localsettings.js
@@ -1,5 +1,9 @@
 "use strict";
 exports.setup = function(parsoidConfig) {
+	// Use the API backends directly without hitting the text varnishes.
+	// API requests are not cacheable anyway.
+	parsoidConfig.defaultAPIProxyURI = 'http://api.svc.eqiad.wmnet';
+
 	// Sample these verbose logs to prevent overwhelm
 	// 1% and 2% for empty/tr and empty/li is based on
 	// seeing the volume in rt-testing.
